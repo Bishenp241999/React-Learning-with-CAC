329b04338c868ab2f5bd0e62c98c4880f2bb4b2b
Bishenp241999 <praptibishen1999@gmail.com>
2024-05-17 22:33:33 +0530

    xx

---
+++ 03-props-tailwind/src/components/UI/Button.jsx
@@ -0,0 +1,9 @@
+// export default function Button({color,...props}){
+//     return(
+//         <button
+//         className="outline-none px-4 py-1 rounded-full shadow-lg text-black bg-red-600 {props}" style={{backgroundColor:classes}}
+//         onClick={() => handleButton("red")}>
+//             {children}
+//         </button>
+//     )
+// }
--- 04-Hooks-and-Project-PasswordGenerator/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <h1>Initial</h1>
-    </>
-  )
-}
-
-export default App
